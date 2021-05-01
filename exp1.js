5b233ea7db49d25139bc405310e4f016dea602b6
Shruti Tripathi <68679259+shrutitripathi13@users.noreply.github.com>
2021-05-01 23:57:17 +0530

    JS programs

--- exp1.js
+++ exp1.js
@@ -1,3 +1,41 @@
+/* first JS program */
 const monthlyRent = 500;
 const yearlyRent = monthlyRent * 12;
 console.log (yearlyRent);
+
+/* program of if-statement */
+const skyIsBlue = true;
+if (skyIsBlue){
+    console,log("The sky is blue!");
+} else {
+    console.log("The sky is not blue!");
+}
+
+/* program of while-loop */
+var students = ["Matt", "Sarah", "Susan"];
+while (students.lenght>0) {
+    let students = students.pop();
+    console.log(`Hello, ${students}`);
+}
+
+/* program of for-loop */
+const character = 'a';
+const timesToRepeat = 50;
+let answer = ' ';
+for ( let i=0; i<=timesToRepeat; i++){
+    answer += character;
+}
+console.log(answer);
+
+/* eample of a function */
+const myHomeCity = "Salt Lake City";
+const myHomeState = "Utah";
+const MyHomeCountry = "USA";
+function logOutYourHouse (city, state, country){
+    console.log('You are from ${city},${state},${country}');
+}
+logOutYourHouse(myHomeCity,myHomeState,MyHomeCountry);
+
+
+
+
